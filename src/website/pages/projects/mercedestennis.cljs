aba620a7437097b536b4849da16835819fd2bdec
Sam <sam@gates.dev>
2024-06-18 11:16:04 -0400

    updating us open page

--- src/website/pages/projects/mercedestennis.cljs
+++ src/website/pages/projects/mercedestennis.cljs
@@ -1,10 +1,5 @@
 (ns website.pages.projects.mercedestennis
  (:require 
-  [reagent.core :as r]
-  [reagent.dom :as rdom]
-  [quil.core :as q]
-  [quil.middleware :as m]
-  ["prismjs/prism.js" :as prism]
   [website.pages.common :refer [link iframe canvas]]))
 
 
@@ -13,10 +8,13 @@
 (def date "June 2019")
 (def body 
   [:div {:style {:font-size 18 :line-height 1.4}} 
+   [:img {:src "/turmolt.github.io/public/resources/US_Open.png"
+             :style {:width 660 :height 660 :margin-top 20 :float :center}}]
+   [:br] [:br] "I developed a Mixed Reality Tennis application showcased at the 2019 US Open. This multiplayer application featured multiple participants competing with dynamic visual effects."
+   [:br] [:br] "The application was built in Unity and utilized a variety of screens and mixed reality peripherals. The setup included two stations with VR tracking pucks attached to tennis rackets, a tablet, and a large LED wall displaying the game state with prominent visual effects. Communication between devices was handled by a custom TCP Client/Server plugin I developed."
+   [:br][:br]
    [iframe {:src "https://www.youtube.com/embed/FbrmlQQ7f9M"
-            :name "Omega VR"}]
-   "I developed a Mixed Reality Tennis application showcased at the 2019 US Open. This multiplayer application featured multiple participants competing with dynamic visual effects."
-   [:br] [:br]"The setup included two stations with VR tracking pucks attached to tennis rackets, a tablet, and a large LED wall displaying the game state with prominent visual effects. Communication between devices was handled by a custom TCP Client/Server plugin I developed."
+            :name "US Open Virtual Tennis"}] 
    [:br] [:br] "I was responsible for all aspects of the project except swing detection, including gameplay, networking, and visual effects. Using Houdini and Substance Designer, I created textures and flipbooks for the application."
    [:br] [:br] "I played a significant role in game design, handling all gameplay system design and implementation."
    [:br] [:br] "The top half of the screen shows the LED wall display, while the bottom half shows one of the player’s screens."])
@@ -26,20 +24,6 @@
 
 (defn icon []
   [:img {:src "/turmolt.github.io/public/resources/US_Open.png" :style {:width 60 :height 60 :margin 10 :margin-top 20 :float :left}}])
-
-(defn preview []
-  [:div {:class "f400"
-         :style {:border-top "1px solid black"
-                 :width "100%"
-                 :height 100}}
-   [:h3
-   {:style {:margin "15px 0px 0px 0px"}} 
-    title]
-   [:p {:style {:font-size 15
-                :margin "10px 0 0 0"
-                :font-style "italic"}}
-    body-preview]])
-
 
 (defn panel []
   (set! (. js/document -title) title)
